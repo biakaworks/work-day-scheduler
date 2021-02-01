3756ef5833993ef3cde07b8fc1346351e32c1ca0
Biaka Z <biaka@biakaworks.com>
2021-01-31 21:19:55 -0600

    Updated HTML with Rows, Times, Buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+// Users can enter their notes in the text area
+// Notes can be saved into local storage
+// If Notes are saved in local storage, its still there when the page reloads
+// Past area is gray
+
