4df3d21c97b282539210a0e40eae3333aafeecf4
Biaka Z <biaka@biakaworks.com>
2021-02-02 01:22:06 -0600

    Testing out Script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const moment = require("./moment");
 // Past area is gray
 var currentDay = moment().format('MMMM Do YYYY');
 var currentTime = moment().format("HH");
-$("#currentday").text(today);
+$("#currentday").text(currentDay);
 
 // For the sections of each time
 var timeOfDay = ["09","10","11","12","01","02","03","04","05"];
@@ -26,3 +26,12 @@ function timeColor() {
     }
 }
 timeColor();
+
+// Functions for each of the time
+// 9AM Row
+$("#Time9").on("click", function (){
+    var task = $("#Time9").val();
+    console.log(task);
+    localStorage.setItem("9:00", task);
+});
+$("#Row10").val(localStorage.getItem("9:00"));
